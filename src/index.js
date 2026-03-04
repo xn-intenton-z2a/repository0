94d1dde2a8ff4a7a31cde7a0270821f4c44317cd
Antony at Polycode <antony@polycode.co.uk>
2026-03-04 22:12:07 +0000

    chore: simplify exports in index.js by removing unused components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@
 // Copyright (C) 2025-2026 Polycode Limited
 // src/index.js - Library entry point
 
-export { 
-  ExpressionParser, 
-  TimeSeriesGenerator, 
-  PlotGenerator, 
-  PlotCodeLib 
+export {
 } from './lib/main.js';
 
 export { PlotCodeLibDemo } from './demo.js';
