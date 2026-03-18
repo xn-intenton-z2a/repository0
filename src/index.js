0faa56a1b445476a1162197705f7621dfd16a6c7
Antony Cartwright <antonyccartwright@gmail.com>
2026-03-18 23:23:52 +0000

    Delete src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (C) 2025-2026 Polycode Limited
-// src/index.js - Library entry point
-
-export { fizzValue, fizzBuzz } from './lib/main.js';
-
-export { PlotCodeLibDemo } from './demo.js';
